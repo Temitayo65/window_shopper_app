663607f7c29f9d80e5e62ec9525b0d7ca8a93706
Ilori Temitayo <iloritemitayo75@gmail.com>
2020-11-12 22:45:26 +0100

    Change design device to iPhone8+

---
+++ window_shopper/View/CustomTextField.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTextField.swift
+//  window_shopper
+//
+//  Created by owner on 12/11/2020.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
